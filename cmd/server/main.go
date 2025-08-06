4ccde6639c81fe471e349eb491be8a4c1cc1637f
aleksandrgaliev <alegaliev@gmail.com>
2025-08-06 16:43:54 +0300

    add iter1 exercise
    add iter1 exercise

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,18 @@
 package main
 
-func main() {}
+import (
+	"github.com/AleGaliev/kubercontroller/internal/handler"
+	"net/http"
+)
+
+func main() {
+	var h handler.MyHandler
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/update/", h.ServeHTTP)
+
+	err := http.ListenAndServe(`:8080`, mux)
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ internal/handler/handler.go
@@ -0,0 +1,61 @@
+package handler
+
+import (
+	"fmt"
+	models "github.com/AleGaliev/kubercontroller/internal/model"
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+type MyHandler struct{}
+
+func (h MyHandler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodPost {
+		res.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	pathUrl := strings.Split(strings.Trim(req.URL.Path, "/"), "/")
+
+	if len(pathUrl) < 4 {
+		res.WriteHeader(http.StatusNotFound)
+		return
+	}
+	name := pathUrl[2]
+	MType := pathUrl[1]
+	value := pathUrl[3]
+
+	switch MType {
+	case models.Gauge:
+		f, err := strconv.ParseFloat(value, 64)
+		if err != nil {
+			fmt.Println(err)
+			res.WriteHeader(http.StatusBadRequest)
+		}
+		models.MemStorage[name] = models.Metrics{
+			ID:    name,
+			MType: MType,
+			Value: &f,
+		}
+
+	case models.Counter:
+		i, err := strconv.ParseInt(value, 10, 64)
+		if err != nil {
+			res.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		if metrics, exists := models.MemStorage[name]; exists {
+			*metrics.Delta += i
+		} else {
+			models.MemStorage[name] = models.Metrics{
+				ID:    name,
+				MType: MType,
+				Delta: &i,
+			}
+		}
+	default:
+		fmt.Printf("Unknown metric type: %s\n", pathUrl[2])
+		res.WriteHeader(http.StatusBadRequest)
+	}
+}
